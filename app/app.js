07944fd48d35e687b0a8507c86a850955efbcc97
grafiaviles <grafiaviles@gmail.com>
2016-12-05 22:08:10 -0300

    Ejercicio 1 unput

--- app/app.js
+++ app/app.js
@@ -10,5 +10,18 @@ angular.module('miapp', ['ui.router'])
 })
 angular.module('miapp')
   .controller('ListController', function($scope) {
-    $scope.todos = [{id: 1, desc: 'Todo 1'}, {id: 2, desc: 'Todo 2'}]
+    $scope.todos = [
+      {id: 1, desc: 'Todo 1', completado: true},
+      {id: 2, desc: 'Todo 2', completado: false}
+    ]
+    $scope.desc = ''
+
+    $scope.agregar = function agregar () {
+      $scope.todos.push({
+        id: (new Date()).getTime(),
+        desc: $scope.desc,
+        completado: false,
+      })
+      $scope.desc = ''
+    }
   })
